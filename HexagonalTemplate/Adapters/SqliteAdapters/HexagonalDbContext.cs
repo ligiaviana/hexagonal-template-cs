173bccc267f3c43ca2d3a78b5138aa345a2913e3
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-05-24 10:13:21 -0300

    12: HexagonalDbContext class created; Implemented <UserEntity> instead of <UserDto>.

---
+++ HexagonalTemplate/Adapters/SqliteAdapters/HexagonalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HexagonalTemplate.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HexagonalTemplate.Adapters.SqliteAdapters
+{
+    public class HexagonalDbContext : DbContext
+    {
+        public HexagonalDbContext(DbContextOptions<HexagonalDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
