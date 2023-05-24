2735979653c1ab3a403e837ca2401d982ee21ca3
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-05-24 10:03:17 -0300

    11: Folders CoreTests, UseCaseTests and Population created; LogCoreTests and JwtCoreTests classes implemented.

---
+++ HexagonalTemplate.Test/CoreTests/LogCoreTests.cs
@@ -0,0 +1,42 @@
+﻿using HexagonalTemplate.Cores;
+using HexagonalTemplate.Ports.Ins;
+using Xunit;
+using Xunit.Abstractions;
+using System.IO;
+using System.Text;
+
+namespace HexagonalTemplate.Test.CoreTests
+{
+    public class LogCoreTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public LogCoreTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Log_ValidMessage_WritesToConsole()
+        {
+            // Arrange
+            string message = "Test message";
+            var logCore = new LogCore();
+            var stringBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(stringBuilder);
+
+            // Redirect Console output to the StringBuilder
+            Console.SetOut(stringWriter);
+
+            // Act
+            logCore.Log(message);
+
+            // Assert
+            var consoleOutput = stringBuilder.ToString();
+            Assert.Contains(message, consoleOutput);
+
+            // Restore Console output
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+        }
+    }
+}
