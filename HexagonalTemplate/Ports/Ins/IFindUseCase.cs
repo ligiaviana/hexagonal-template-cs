988553b11bea98e183c9ba686e657ca07e2ecf65
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-06-02 11:25:24 -0300

    23: Refactoring of the “GetUserByEmail” method of FindUseCase because the UserHttpsAdapter class handles the Status code 200, 404 and 500 in the try-catch blocks, not being necessary to repeat in FindUseCase. Note: Test Results on Postman passed.

--- HexagonalTemplate/Ports/Ins/IFindUseCase.cs
+++ HexagonalTemplate/Ports/Ins/IFindUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using HexagonalTemplate.Models.Dtos;
 using HexagonalTemplate.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HexagonalTemplate.Ports.Ins
 {
--- HexagonalTemplate/UseCases/FindUseCase.cs
+++ HexagonalTemplate/UseCases/FindUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using HexagonalTemplate.Models.Entities;
 using HexagonalTemplate.Ports.Ins;
 using HexagonalTemplate.Ports.Outs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HexagonalTemplate.UseCases
 {
@@ -12,13 +13,32 @@ namespace HexagonalTemplate.UseCases
         {
             this.userRepository = userRepository;
         }
+        //public ActionResult GetUserByEmail(string email)
+        //{
+        //    try
+        //    {
+        //        var user = userRepository.FindByEmail(email);
+
+        //        if (user == null)
+        //        {
+        //            return new StatusCodeResult(404);
+        //        }
+        //        return new OkObjectResult(user);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return new StatusCodeResult(500);
+        //    }
+        //}
+
         public UserEntity GetUserByEmail(string email)
         {
+
             var user = userRepository.FindByEmail(email);
 
             if (user == null)
             {
-                return null;
+                throw new ArgumentNullException("User not found");
             }
             return user;
         }
